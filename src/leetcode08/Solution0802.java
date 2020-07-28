1197c07be285beebd30e116bc4745e13de95ec3f
jiaoery <373105324@qq.com>
2020-07-28 17:41:02 +0800

    问题8：求解树形结构的最大深度 方法2

--- src/leetcode08/Solution0802.java
+++ src/leetcode08/Solution0802.java
@@ -1,5 +1,8 @@
 package leetcode08;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @ClassName: Solution0802.java
  * @Description:TODO(用一句话描述该文件做什么) 
@@ -9,5 +12,31 @@ package leetcode08;
  * @Date    2020年7月28日 下午3:18:58 
  */
 public class Solution0802 {
+	
+	public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            ans++;
+        }
+        return ans;
+    }
+
+
 
 }

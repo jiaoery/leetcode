a73f819e0a3868bd8f9e362909d61f595d6eb25c
jiaoery <373105324@qq.com>
2020-07-31 16:40:51 +0800

    处理冲突，重新提交

--- src/leetcode10/Solution10.java
+++ src/leetcode10/Solution10.java
@@ -13,11 +13,7 @@ import javax.management.RuntimeErrorException;
 public class Solution10 {
 	
 	public int findMagicIndex(int[] nums) {
-<<<<<<< HEAD
 		if(nums.length<1||nums.length>1000000) {
-=======
-		if(nums.length<1||nums.length>10000) {
->>>>>>> aa6d939264971cc3540034f3a68737c93c542f31
 			throw new RuntimeException("array's length out of limit");
 		}
         for(int i =0;i<nums.length;i++){

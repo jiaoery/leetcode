ae4b73c8f9cb739cb0b522c1b054e681613ebfd2
jiaoery <373105324@qq.com>
2020-08-18 17:34:34 +0800

    Question21：求解错误的集合

--- src/leetcode21/Solotion2104.java
+++ src/leetcode21/Solotion2104.java
@@ -9,5 +9,19 @@ package leetcode21;
  * @Date    2020年8月18日 下午1:59:21 
  */
 public class Solotion2104 {
+	 public int[] findErrorNums(int[] nums) {
+	        int dup = -1, missing = 1;
+	        for (int n: nums) {
+	            if (nums[Math.abs(n) - 1] < 0)
+	                dup = Math.abs(n);
+	            else
+	                nums[Math.abs(n) - 1] *= -1;
+	        }
+	        for (int i = 1; i < nums.length; i++) {
+	            if (nums[i] > 0)
+	                missing = i + 1;
+	        }
+	        return new int[]{dup, missing};
+	    }
 
 }
--- src/leetcode21/Solution2103.java
+++ src/leetcode21/Solution2103.java
@@ -1,13 +1,29 @@
 package leetcode21;
 
+import java.util.HashMap;
+
 /**
  * @ClassName: Solution2103.java
- * @Description:TODO(用一句话描述该文件做什么) 
+ * @Description:使用map存储
  * 
  * @author  jixiang
  * @version v1.0.0 
  * @Date    2020年8月18日 上午11:22:02 
  */
 public class Solution2103 {
-
+	public int[] findErrorNums(int[] nums) {
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int dup =-1,missing=1;
+		for(int n:nums) {
+			 map.put(n, map.getOrDefault(n, 0) + 1);
+		}
+		for (int i = 1; i <= nums.length; i++) {
+            if (map.containsKey(i)) {
+                if (map.get(i) == 2)
+                    dup = i;
+            } else
+                missing = i;
+        }
+        return new int[]{dup, missing};
+	}
 }

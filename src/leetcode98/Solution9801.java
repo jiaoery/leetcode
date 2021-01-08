fce618d2e176a2329dd50843fa868b52a634aeb5
jiaoery <37305324@qq.com>
2021-01-08 18:01:13 +0800

    Question98：最长特殊序列I

--- src/leetcode98/Solution9801.java
+++ src/leetcode98/Solution9801.java
@@ -2,4 +2,10 @@ package leetcode98;
 
 public class Solution9801 {
 
+	public int findLUSlength(String a, String b) {
+		if (a.equals(b)) {
+			return -1;
+		}
+		return Math.max(a.length(), b.length());
+	}
 }
